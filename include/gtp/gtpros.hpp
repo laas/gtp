2411477f5c27174c20ff1c486b5fadec12eac834
Jules Waldhart <jules.waldhart@laas.fr>
2016-11-15 14:03:31 +0100

    bugfix

--- include/gtp/gtpros.hpp
+++ include/gtp/gtpros.hpp
@@ -13,6 +13,7 @@
 #include <gtp_ros_msgs/PlanAction.h>
 #include <gtp_ros_msgs/PublishTraj.h>
 #include <gtp_ros_msgs/GetDetails.h>
+#include <trajectory_msgs/JointTrajectory.h>
 
 #include <message_filters/subscriber.h>
 #include <message_filters/sync_policies/exact_time.h>
